946da4525b751bae96261ef716b78e14637e7ee1
Benson Sherman <bcs4985@rit.edu>
2017-11-08 10:39:44 -0500

    creAted end goal prefab

---
+++ Assets/Scripts/GoalEnter.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoalEnter : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    /// <summary>
+    /// Checks to see if the player enters
+    /// If they do the player wins
+    /// </summary>
+    /// <param name="other">Collider for the gameobject entering the trigger</param>
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            Debug.Log("You win, you dork!");
+        }
+    }
+}
