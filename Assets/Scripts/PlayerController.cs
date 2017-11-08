41d3bb663c73b71f2926e84efeff550faac72943
Aidan Markham <apm4337@rit.edu>
2017-11-08 11:06:32 -0500

    added to The movement

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    public float speed;
+    public float rotationSpeed;
+    private Rigidbody2D rb;
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+	}
+
+    // Update is called once per frame
+    void Update() {
+        Vector2 direction = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")).normalized;
+
+        rb.AddForce(direction * speed * Time.deltaTime);
+
+
+        
+	}
+}
