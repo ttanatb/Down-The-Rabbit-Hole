4874391f1cb21db691c4eff3c4870dfbcf13976f
MAIN\aeh8533 <aeh8533@g.rit.edu>
2017-11-10 11:10:03 -0500

    Enemy Script and my own test scene.

---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    #region EnemyType
+    // Enum for the EnemyType Variable
+    enum EnemyType
+    {
+        Keeper, // Eye creature that rotates
+        Dhole, // Moves to direct path spots
+        Slug, // Slow path
+        Beast, // Path or stationary, has a hearing radius
+        Haunter,
+    }
+    #endregion
+
+    #region Variables
+    // Set this 
+    public EnemyType enemyType;
+    public List<GameObject> pathPoints;
+    #endregion
+
+    #region Update
+    // Update is called once per frame
+    void Update ()
+    {
+        EnemyMovement();
+	}
+    #endregion
+
+    #region Enemy Movement Helper Method
+    /// <summary>
+    /// Movement method that is called for enemy Movement
+    /// </summary>
+    void EnemyMovement()
+    {
+        switch(enemyType)
+        {
+            case EnemyType.Keeper:
+                // Rotate
+                break;
+
+            case EnemyType.Dhole:
+                // Go to different spots, rotate slightly?
+                break;
+
+            case EnemyType.Slug:
+                // Path follow
+                break;
+
+            case EnemyType.Beast:
+                // Path follow if given a path
+                // Stationary otherwise
+                break;
+
+            case EnemyType.Haunter:
+                // Stationary?
+                break;
+        }
+    }
+    #endregion
+}
