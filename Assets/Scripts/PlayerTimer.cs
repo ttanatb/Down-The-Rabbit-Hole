ab6a00c816672a59b9ff60a2ebcbbdb850f73c3f
Aidan Markham <apm4337@rit.edu>
2017-11-14 19:54:35 -0500

    Created PlayerTimer script

---
+++ Assets/Scripts/PlayerTimer.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerTimer : MonoBehaviour {
+    
+    //float to hold how much time has passed thusfar
+    public float timeScore;
+
+    //true if the player is currently playing, false if it should be stopped
+    public bool timePaused;
+
+    //bool to hold if the player is at the beginning or not
+    public bool runStarted;
+
+    //the player
+    public GameObject player;
+
+
+
+	// Use this for initialization
+	void Start () {
+        runStarted = false;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if(player.GetComponent<Rigidbody>().velocity.magnitude > 0)
+        {
+            //start the run if the player is moving
+            runStarted = true;
+            
+        }
+
+
+		if(!timePaused && runStarted)
+        {
+            //update time
+            timeScore += Time.deltaTime;
+        }
+	}
+
+    void End()
+    {
+        //keep the players time score from increasing
+        timePaused = true;
+    }
+}
