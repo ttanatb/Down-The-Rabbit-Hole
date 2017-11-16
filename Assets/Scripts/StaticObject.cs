bf54ce1a1638f876946b5db3dd896c92ecf02900
MAIN\bcs4985 <bcs4985>
2017-11-16 14:16:30 -0500

    Implemented SceneChange

---
+++ Assets/Scripts/StaticObject.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StaticObject : MonoBehaviour
+{
+    SceneChange sceneChange;
+    // Use this for initialization
+    void Start()
+    {
+        sceneChange = GameObject.Find("SceneManager").GetComponent<SceneChange>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player"&& gameObject.tag =="Goal")
+            sceneChange.ChangeState(SceneChange.SceneState.Win);
+        
+    }
+}
