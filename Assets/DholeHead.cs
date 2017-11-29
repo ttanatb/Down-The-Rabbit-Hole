3f59ba1c2131d17623257b0eb2652add5dc43166
ttanatb <ttanatb@gmail.com>
2017-11-29 02:38:00 -0500

    Added dhole and its animations

---
+++ Assets/DholeHead.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class DholeHead : MonoBehaviour
+{
+
+    private Transform root;
+
+    private void Start()
+    {
+        root = transform.root;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.right = root.up;
+    }
+}
